33902e640c6dd6a3fe14fb9a042538a4bbfb10f8
Lee Richardson <github@leerichardson.com>
2017-01-11 23:13:34 -0500

    Finished color wheel

--- EasyLayout.Droid.Sample/Colors.cs
+++ EasyLayout.Droid.Sample/Colors.cs
@@ -9,9 +9,11 @@ namespace EasyLayout.Droid.Sample
         public static readonly Color Orange = Color.Rgb(241, 142, 28);
         public static readonly Color Yellow = Color.Rgb(244, 229, 0);
         public static readonly Color Green = Color.Rgb(0, 142, 91);
-        public static readonly Color Blue = Color.Rgb(42, 113, 176);
+        public static readonly Color DarkBlue = Color.Rgb(42, 113, 176);
+        public static readonly Color LightBlue = Color.Rgb(6, 150, 187);
         public static readonly Color Purple = Color.Rgb(109, 57, 139);
         public static readonly Color DarkGrey = Color.Rgb(20, 20, 20);
         public static readonly Color Black = Color.Rgb(0, 0, 0);
+        public static readonly Color BluePurple = Color.Rgb(68, 78, 153);
     }
 }
--- EasyLayout.Droid.Sample/MainActivity.cs
+++ EasyLayout.Droid.Sample/MainActivity.cs
@@ -15,6 +15,9 @@ namespace EasyLayout.Droid.Sample
         private TextView _left;
         private TextView _bottom;
         private TextView _upperRight;
+        private TextView _upperLeft;
+        private TextView _lowerLeft;
+        private TextView _lowerRight;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -29,11 +32,14 @@ namespace EasyLayout.Droid.Sample
         {
             _relativeLayout = ViewUtils.AddRelativeLayout(this);
             _center = _relativeLayout.AddTextView(this, "this.GetCenter() == \nrelativeLayout.GetCenter()", Colors.DarkGrey, Colors.White);
-            _top = _relativeLayout.AddTextView(this, "this.Bottom == \n_center.Top - 20", Colors.Yellow, Colors.Black);
+            _top = _relativeLayout.AddTextView(this, "this.Bottom == \n_center.Top - 20", Colors.Yellow, Colors.DarkGrey);
             _right = _relativeLayout.AddTextView(this, "this.Left == \n_center.Right + 20", Colors.Red, Colors.White);
-            _left = _relativeLayout.AddTextView(this, "this.Right == \n_center.Left - 20", Colors.Blue, Colors.White);
-            _bottom = _relativeLayout.AddTextView(this, "this.Top == \n_center.Bottom + 20", Colors.Purple, Colors.White);
-            _upperRight = _relativeLayout.AddTextView(this, "this.Height == 40 &&\nthis.Width == 140", Colors.Orange, Colors.White);
+            _left = _relativeLayout.AddTextView(this, "this.Right == \n_center.Left - 20", Colors.LightBlue, Colors.White);
+            _bottom = _relativeLayout.AddTextView(this, "this.Top == \n_center.Bottom + 20", Colors.BluePurple, Colors.White);
+            _upperRight = _relativeLayout.AddTextView(this, "this.Baseline ==\n_top.Baseline", Colors.Orange, Colors.White);
+            _upperLeft = _relativeLayout.AddTextView(this, "this.Height == 40 &&\nthis.Width == 140", Colors.Green, Colors.White);
+            _lowerLeft = _relativeLayout.AddTextView(this, "this.Right == _left.Right &&\nthis.Bottom == _bottom.Bottom", Colors.DarkBlue, Colors.White);
+            _lowerRight = _relativeLayout.AddTextView(this, "this.Top == _bottom.Top &&\nthis.Left == _right.Left", Colors.Purple, Colors.White);
         }
 
         private void ConstrainLayout()
@@ -58,9 +64,20 @@ namespace EasyLayout.Droid.Sample
                 _bottom.Top == _center.Bottom + 20 &&
 
                 _upperRight.Left == _center.Right + 20 &&
-                _upperRight.Bottom == _center.Top - 20 &&
-                _upperRight.Height == 40 &&
-                _upperRight.Width == 140
+                _upperRight.Baseline == _top.Baseline &&
+                _upperRight.Height == 45 &&
+                _upperRight.Width == 140 &&
+
+                _upperLeft.Right == _center.Left - 20 &&
+                _upperLeft.Bottom == _center.Top - 20 &&
+                _upperLeft.Height == 40 &&
+                _upperLeft.Width == 140 &&
+
+                _lowerLeft.Right == _left.Right &&
+                _lowerLeft.Bottom == _bottom.Bottom &&
+
+                _lowerRight.Top == _bottom.Top &&
+                _lowerRight.Left == _right.Left
                 );
         }
     }
