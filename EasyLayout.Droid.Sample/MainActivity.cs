c881d53a5f69b7ef292d7263f16d52f8208a6b75
Lee Richardson <github@leerichardson.com>
2017-01-11 22:33:47 -0500

    Colors and a Top text

--- EasyLayout.Droid.Sample/MainActivity.cs
+++ EasyLayout.Droid.Sample/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.Widget;
 using Android.OS;
 using Android.Views;
@@ -10,8 +11,10 @@ namespace EasyLayout.Droid.Sample
     [Activity(Label = "EasyLayout.Droid.Sample", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
         private RelativeLayout _relativeLayout;
-        private TextView _centerText;
+        private TextView _center;
+        private TextView _top;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -28,14 +31,18 @@ namespace EasyLayout.Droid.Sample
         private void AddViews()
         {
             _relativeLayout = ViewUtils.AddRelativeLayout(this);
-            _centerText = _relativeLayout.Add<TextView>(this);
-            _centerText.Text = "Hello World";
+            _center = _relativeLayout.AddTextView(this, "_center.GetCenter() == \nrelativeLayout.GetCenter()", Colors.DarkGrey, Colors.White);
+            _top = _relativeLayout.AddTextView(this, "_top.Bottom == \n_center.Top + 20", Colors.Yellow, Colors.Black);
         }
 
         private void ConstrainLayout()
         {
             _relativeLayout.ConstrainLayout(() =>
-                _centerText.GetCenter() == _relativeLayout.GetCenter()
+                _center.GetCenter() == _relativeLayout.GetCenter() &&
+
+                _top.Left == _center.Left &&
+                _top.Right == _center.Right &&
+                _top.Bottom == _center.Top + 20
                 );
         }
     }
@@ -59,6 +66,28 @@ namespace EasyLayout.Droid.Sample
             parent.AddView(child);
             return child;
         }
+
+        public static TextView AddTextView(this ViewGroup parent, Context context, string text, Color background, Color textColor)
+        {
+            var textView = parent.Add<TextView>(context);
+            textView.Text = text;
+            textView.SetBackgroundColor(background);
+            textView.SetTextColor(textColor);
+            return textView;
+        }
+    }
+
+    public static class Colors
+    {
+        public static readonly Color White = Color.Rgb(255, 255, 255);
+        public static readonly Color Red = Color.Rgb(227, 35, 34);
+        public static readonly Color Orange = Color.Rgb(241, 142, 28);
+        public static readonly Color Yellow = Color.Rgb(244, 229, 0);
+        public static readonly Color Green = Color.Rgb(0, 142, 91);
+        public static readonly Color Blue = Color.Rgb(42, 113, 176);
+        public static readonly Color Purple = Color.Rgb(109, 57, 139);
+        public static readonly Color DarkGrey = Color.Rgb(20, 20, 20);
+        public static readonly Color Black = Color.Rgb(0, 0, 0);
     }
 }
 
