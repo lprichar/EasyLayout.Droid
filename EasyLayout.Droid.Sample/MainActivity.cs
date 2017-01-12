93b5ab595775f6dea081a542a81dfd5a729c4a4d
Lee Richardson <github@leerichardson.com>
2017-01-11 21:53:55 -0500

    Blank sample app

---
+++ EasyLayout.Droid.Sample/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace EasyLayout.Droid.Sample
+{
+    [Activity(Label = "EasyLayout.Droid.Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
